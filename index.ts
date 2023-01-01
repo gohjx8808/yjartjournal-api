1306972700b75ec5e7eac3c15f4e774d5938a055
jingxuangoh <gohjx8808@gmail.com>
2023-01-01 23:11:25 +0800

    change vercel.json content

--- index.ts
+++ index.ts
@@ -15,12 +15,8 @@ app.get('/', (_req, _res) => {
 
 app.use('/products', productRouter);
 
-dataSource.initialize();
-
 app.listen(port, async () => {
   await dataSource.initialize();
   await dataSource.runMigrations();
   console.log(`TypeScript with Express http://localhost:${port}/`);
 });
-
-module.exports = app;
