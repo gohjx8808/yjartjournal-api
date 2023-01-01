4a9ef6869f10e68fdd9068ce326ab0457f1fcd26
jingxuangoh <gohjx8808@gmail.com>
2023-01-01 09:05:24 +0800

    change to postgresql

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -3,12 +3,12 @@ import { SeedSortOptions1672062650913 } from '../migrations/1672062650913-SeedSo
 import SortOptions from './entities/SortOptions';
 
 export const dataSource = new DataSource({
-  type: 'mysql', 
-  host: process.env.MY_SQL_DB_HOST,
-  port:  parseInt(process.env.MY_SQL_DB_PORT || '0', 10),
-  username: process.env.MY_SQL_DB_USER,
-  password: process.env.MY_SQL_DB_PASSWORD,
-  database: process.env.MY_SQL_DB_DATABASE,
+  type: 'postgres', 
+  host: process.env.POSTGRESQL_DB_HOST,
+  port:  parseInt(process.env.POSTGRESQL_DB_PORT || '0', 10),
+  username: process.env.POSTGRESQL_DB_USER,
+  password: process.env.POSTGRESQL_DB_PASSWORD,
+  database: process.env.POSTGRESQL_DB_DATABASE,
   entities: [
     SortOptions,
   ],
