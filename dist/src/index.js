bb1fc92dd074af642dcb86b3decc7b350435f987
jingxuangoh <gohjx8808@gmail.com>
2023-01-04 18:34:27 +0800

    try init app after db initialization

--- dist/src/index.js
+++ dist/src/index.js
@@ -9,13 +9,13 @@ const dataSource_1 = require("./dataSource");
 const productRouter_1 = require("./routers/productRouter");
 dataSource_1.dataSource.initialize().then(() => {
     dataSource_1.dataSource.runMigrations();
+    const app = (0, express_1.default)();
+    const port = 3000;
+    app.use(express_1.default.json());
+    app.get('/', (_req, _res) => {
+        _res.send('TypeScript With Express');
+    });
+    app.use('/products', productRouter_1.productRouter);
+    app.listen(port);
 });
-const app = (0, express_1.default)();
-const port = 3000;
-app.use(express_1.default.json());
-app.get('/', (_req, _res) => {
-    _res.send('TypeScript With Express');
-});
-app.use('/products', productRouter_1.productRouter);
-app.listen(port);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,19 @@ import { productRouter } from './routers/productRouter';
 
 dataSource.initialize().then(()=>{
   dataSource.runMigrations();
+  const app: express.Application = express();
+
+  const port: number = 3000;
+
+  app.use(express.json());
+
+  app.get('/', (_req, _res) => {
+    _res.send('TypeScript With Express');
+  });
+
+  app.use('/products', productRouter);
+
+  app.listen(port);
 });
 
-const app: express.Application = express();
 
-const port: number = 3000;
-
-app.use(express.json());
-
-app.get('/', (_req, _res) => {
-  _res.send('TypeScript With Express');
-});
-
-app.use('/products', productRouter);
-
-app.listen(port);
